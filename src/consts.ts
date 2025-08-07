90518e4dc07c2c49d26478c23f246c5d5d1a3cda
JaydenWN <123134488+JaydenWN@users.noreply.github.com>
2025-08-07 11:24:21 +1000

    updated url

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'PVC Fencing Central Coast | Noreast Fencing and Gates';
+export const SITE_DESCRIPTION = 'Noreast Fencing and Gates are the Central Coast PVC Fencing Experts.';
